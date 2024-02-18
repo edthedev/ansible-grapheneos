55dc47f131ff4ba082c04fea8dbfdf44e18b4f2e
EdTheDev <edward@delaporte.us>
2024-02-18 16:39:18 -0600

    Add finish warning

--- install-to-phone.sh
+++ install-to-phone.sh
@@ -1,7 +1,10 @@
-$PATH=$HOME/bin/android_platform_tools/platform-tools:$PATH
+export PATH="$HOME/bin/android_platform_tools/platform-tools:$PATH"
 read -p "On your phone, unlock OEM (Developer Settings / OEM unlocking), then boot into the bootloader interface (power+vol down), then press Enter"
+read -p "Do not unplug your phone when you see 'Finished'. You will see that a lot."
+read -p "Wait for <<<FINISHED FOR REAL>>>"
 fastboot flashing unlock
 cd $HOME/graphene_files/lynx-factory-2024020500
 ./flash-all.sh
 fastboot flashing lock
+read -p "<<<FINISHED FOR REAL>>>"
 read -p "On your phoe, set the bootloader to locked, then unplug and reboot and enjoy GrpaheneOS."
