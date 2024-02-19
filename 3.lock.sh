8a69aa4a75d89ac180341a6a1d240a32155a9517
EdTheDev <edward@delaporte.us>
2024-02-18 19:06:49 -0600

    SPeling

--- 3.lock.sh
+++ 3.lock.sh
@@ -1,4 +1,4 @@
 export PATH="$HOME/bin/android_platform_tools/platform-tools:$PATH"
 read -p "On your phone, unlock OEM (Developer Settings / OEM unlocking), then boot into the bootloader interface (power+vol down), then press Enter"
 fastboot flashing lock
-read -p "On your phoe, set the bootloader to locked, then unplug and reboot and enjoy GrpaheneOS."
+read -p "On your phone, set the bootloader to locked, then unplug and reboot and enjoy GrpaheneOS."
