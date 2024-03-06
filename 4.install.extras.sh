03ea09a58babe5f8ff919fd9599722ae1c75c528
Edward Delaporte <edward@delaporte.us>
2024-03-05 19:42:04 -0600

    Update 4.install.extras.sh

--- 4.install.extras.sh
+++ 4.install.extras.sh
@@ -3,6 +3,8 @@ echo "Be sure the phone is plugged connected by USB, and USB debugging is enable
 read -p "Be sure the phone is unlocked, and to accept the USB debugging prompt."
 echo "Setting night mode"
 adb shell "cmd uimode night yes"
+echo "No 911 power button around babies"
+adb shell su -c 'pm disable com.android.server.telecom/.PrivilegedCallActivity'
 echo "Installing packages"
 adb install-multi-package -r $HOME/android_apk/*.apk
 echo "If there were errors, run 'adb kill-server' and watch for the prompt on re-run."
